305ff80d0681dd5966bb6886e6b15a2593d749e6
MysterionNH <mysterion.nh@gmail.com>
2015-07-25 11:33:39 +0200

    Refractored RuleOfThreeActivity, made it readable

--- app/src/main/java/io/github/mysterionnh/allmuffin/RuleOfThreeActivity.java
+++ app/src/main/java/io/github/mysterionnh/allmuffin/RuleOfThreeActivity.java
@@ -12,6 +12,9 @@ public class RuleOfThreeActivity extends BaseActivity {
 
     // Count of TextViews in Layout, is important!
     private final int INPUT_FIELD_COUNT = 6;
+    String texts[]  = new String[INPUT_FIELD_COUNT];
+    EditText eTexts[] = new EditText[INPUT_FIELD_COUNT];
+    boolean empty[] = new boolean[INPUT_FIELD_COUNT];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,103 +32,7 @@ public class RuleOfThreeActivity extends BaseActivity {
         btnListener = new View.OnClickListener() {
 
             public void onClick(View v) {
-
-                EditText eTexts[] = {(EditText) findViewById(R.id.currentPercent),
-                        (EditText) findViewById(R.id.currentValue),
-                        (EditText) findViewById(R.id.valueOne),
-                        (EditText) findViewById(R.id.valueHundred),
-                        (EditText) findViewById(R.id.questionedPercent),
-                        (EditText) findViewById(R.id.questionedValue)};
-
-                String texts[] = new String[INPUT_FIELD_COUNT];
-
-                for (int i = 0; i < INPUT_FIELD_COUNT; i++) {
-                    texts[i] = eTexts[i].getText().toString();
-                }
-
-                boolean empty[] = new boolean[texts.length];
-
-                int textCount = 0;
-
-                for (String text : texts) {
-                    empty[textCount] = text.isEmpty();
-
-                    //When it isn't empty in a regular way, checks if it is empty in our way 1
-                    if (!empty[textCount]) {
-                        try {
-                            //noinspection ResultOfMethodCallIgnored
-                            Double.parseDouble(text);
-                        } catch (Exception e) {
-                            empty[textCount] = true;
-                        }
-                    }
-                    ++textCount;
-                }
-
-                //#Easteregg #enterStanleyParableReferenceHere
-                if (eight(texts)) {
-                    openPopup("8",
-                            "",
-                            "8",
-                            Color.RED,
-                            Gravity.CENTER,
-                            75);
-                    return;
-                }
-
-                if (!empty[0] && !empty[1]) {
-                    //Catch division by zero before it happens
-
-                    if (texts[0].equals("0")) {
-                        error();
-                        return;
-                    }
-
-                    //Calculating the value at one percent by dividing the current value by current percent
-                    texts[2] = String.valueOf(Double.parseDouble(texts[1]) / Double.parseDouble(texts[0]));
-
-                    //Calculating the value at one hundred percent by dividing the current value by current percent and multiplying with one hundred #bestWay #nope :D
-                    texts[3] = String.valueOf(100 * (Double.parseDouble(texts[1]) / Double.parseDouble(texts[0])));
-
-                    if (!empty[4]) {
-                        texts[5] = String.valueOf(Double.parseDouble(texts[2]) * Double.parseDouble(texts[4]));
-                    } else if (!empty[5]) {
-                        texts[4] = String.valueOf(Double.parseDouble(texts[5]) / Double.parseDouble(texts[2]));
-                    }
-                } else if (empty[2] && empty[3]) {
-                    error();
-                    return;
-
-                } else if (!empty[2]) {
-
-                    // In this case the first two TextViews are useless, hence I "empty" them
-                    texts[0] = "---";
-                    texts[1] = "---";
-
-                    texts[3] = String.valueOf(100 * Double.parseDouble(texts[2]));
-
-                    // When the last line (calc line) is used, set the not given variable
-                    if (!empty[4]) {
-                        texts[5] = String.valueOf(Double.parseDouble(texts[2]) * Double.parseDouble(texts[4]));
-                    } else if (!empty[5]) {
-                        texts[4] = String.valueOf(Double.parseDouble(texts[5]) / Double.parseDouble(texts[2]));
-                    }
-                } else {
-
-                    // In this case the first two TextViews are useless, hence I "empty" them
-                    texts[0] = "---";
-                    texts[1] = "---";
-
-                    texts[2] = String.valueOf(Double.parseDouble(texts[3]) / 100);
-
-                    // When the last line (calc line) is used, set the not given variable
-                    if (!empty[4]) {
-                        texts[5] = String.valueOf(Double.parseDouble(texts[2]) * Double.parseDouble(texts[4]));
-                    } else if (!empty[5]) {
-                        texts[4] = String.valueOf(Double.parseDouble(texts[5]) / Double.parseDouble(texts[2]));
-                    }
-                }
-                updateFields(texts, eTexts);
+            magicStuff();
             }
         };
     }
@@ -137,13 +44,101 @@ public class RuleOfThreeActivity extends BaseActivity {
         }
     }
 
-    // Just an simple easter egg
-    public boolean eight(String texts[]) {
-        boolean ee = true;
-        for (int i = 0; i < texts.length && ee; i++) {
-            ee = texts[i].equals("8");
+    public void getTexts() {
+        // TODO: Try to get rid of uselessPlaceholder[]
+        // Get all TextFields in Activity, put them into an array
+        EditText uselessPlaceHolder[] = {(EditText) findViewById(R.id.currentPercent),
+                (EditText) findViewById(R.id.currentValue),
+                (EditText) findViewById(R.id.valueOne),
+                (EditText) findViewById(R.id.valueHundred),
+                (EditText) findViewById(R.id.questionedPercent),
+                (EditText) findViewById(R.id.questionedValue)};
+        // Assign to global variable for later use @see updateFields()
+        eTexts = uselessPlaceHolder;
+
+        // Extract the text out of the TextFields
+        for (int i = 0; i < INPUT_FIELD_COUNT; i++) {
+            texts[i] = eTexts[i].getText().toString();
         }
-        return ee;
+    }
+
+    public void areInputsEmpty() {
+        for (int i = 0; i < INPUT_FIELD_COUNT; i++) {
+            empty[i] = texts[i].isEmpty();
+
+            // Okay, there is text in it, buuut is it a double?
+            if (!empty[i]) {
+                try {
+                    //noinspection ResultOfMethodCallIgnored
+                    Double.parseDouble(texts[i]);
+                } catch (Exception e) {
+                    // Nope it isn't, so it's empty for our uses
+                    empty[i] = true;
+                }
+            }
+        }
+        // Okay, done so far, but to avoid a dividing by 0, one last check
+        if (!empty[0]) {
+            empty[0] = (Double.parseDouble(texts[0]) == 0.0);
+        }
+    }
+
+    public void calculate() {
+        // TODO: Clean this up, maybe a switch?
+
+        if (!empty[0] && !empty[1]) {
+
+            //Calculating the value at one percent by dividing the current value by current percent
+            texts[2] = String.valueOf(Double.parseDouble(texts[1]) / Double.parseDouble(texts[0]));
+
+            //Calculating the value at one hundred percent by dividing the current value by current percent and multiplying with one hundred #bestWay #nope :D
+            texts[3] = String.valueOf(100 * (Double.parseDouble(texts[1]) / Double.parseDouble(texts[0])));
+
+            if (!empty[4]) {
+                texts[5] = String.valueOf(Double.parseDouble(texts[2]) * Double.parseDouble(texts[4]));
+            } else if (!empty[5]) {
+                texts[4] = String.valueOf(Double.parseDouble(texts[5]) / Double.parseDouble(texts[2]));
+            }
+        } else if (empty[2] && empty[3]) {
+            // Nothing to calculate, stupid user
+            error();
+
+        } else if (!empty[2]) {
+
+            // In this case the first two TextViews are useless, hence I "empty" them
+            texts[0] = "---";
+            texts[1] = "---";
+
+            texts[3] = String.valueOf(100 * Double.parseDouble(texts[2]));
+
+            // When the last line (calc line) is used, set the not given variable
+            if (!empty[4]) {
+                texts[5] = String.valueOf(Double.parseDouble(texts[2]) * Double.parseDouble(texts[4]));
+            } else if (!empty[5]) {
+                texts[4] = String.valueOf(Double.parseDouble(texts[5]) / Double.parseDouble(texts[2]));
+            }
+        } else {
+
+            // In this case the first two TextViews are useless, hence I "empty" them
+            texts[0] = "---";
+            texts[1] = "---";
+
+            texts[2] = String.valueOf(Double.parseDouble(texts[3]) / 100);
+
+            // When the last line (calc line) is used, set the not given variable
+            if (!empty[4]) {
+                texts[5] = String.valueOf(Double.parseDouble(texts[2]) * Double.parseDouble(texts[4]));
+            } else if (!empty[5]) {
+                texts[4] = String.valueOf(Double.parseDouble(texts[5]) / Double.parseDouble(texts[2]));
+            }
+        }
+    }
+
+    public void magicStuff() {
+        getTexts();
+        areInputsEmpty();
+        calculate();
+        updateFields(texts, eTexts);
     }
 
     public void error() {
@@ -156,6 +151,3 @@ public class RuleOfThreeActivity extends BaseActivity {
                 DEFAULT_TEXT_SIZE);
     }
 }
-
-//TODO : Fix the dividing by 0 errors that are still sitting in there :/
-//TODO : Add rounding!!!1!elf!!
