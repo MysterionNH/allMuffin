3b8194db5baa225e283b498e344c087dc5b493d7
MysterionNH <mysterion.nh@gmail.com>
2015-08-21 14:31:07 +0200

    Fixed a bug in an old project (rot), the rounding feature I added a few days ago was not crash save. Now it is

--- app/src/main/java/com/mysterionnh/allmuffin/activities/RuleOfThreeActivity.java
+++ app/src/main/java/com/mysterionnh/allmuffin/activities/RuleOfThreeActivity.java
@@ -51,7 +51,9 @@ public class RuleOfThreeActivity extends BaseActivity {
     private void updateFields(String[] texts, TextView[] eTexts) {
 
         for (int i = 0; i < texts.length; i++) {
-            eTexts[i].setText(String.valueOf(Math.round(Double.valueOf(texts[i]) * 1000.0) / 1000.0));
+            if (!texts[i].equals("")) {
+                eTexts[i].setText(String.valueOf(Math.round(Double.valueOf(texts[i]) * 1000.0) / 1000.0));
+            }
         }
     }
 
